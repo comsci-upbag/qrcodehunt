095169c1de97012017cc14551a59a95a7c425ea5
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-08-30 14:02:08 +0800

    rewrite main app and install fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Scanner from './Scanner.svelte';
 	import Dock from './Dock.svelte';
+
+	import "@fontsource/poppins";
 </script>
 
 <svelte:head>
@@ -8,44 +10,52 @@
 	<meta name="description" content="COMSCI@UP.BAG" />
 </svelte:head>
 
-<div class="w-[1080px] h-[1920px] relative">
-	<div class="w-[1080px] h-[2276px] left-0 top-[-174px] absolute">
-		<div
-			class="w-[1000px] h-[870px] left-[54px] top-[1406px] absolute bg-violet-700 bg-opacity-50 rounded-full blur-[1000px]"
-		/>
-	</div>
-	<img
-		class="w-[823px] h-[897px] left-[128px] top-[463px] absolute rounded-[20px]"
-		src="https://via.placeholder.com/823x897"
-		alt=""
-	/>
-	<div class="w-[908px] h-[380px] left-[86px] top-[1666px] absolute">
-		<div
-			class="w-[908px] h-[313px] left-0 top-[67px] absolute bg-violet-50 bg-opacity-50 rounded-[40px]"
-		/>
-		<div class="w-[241px] h-[254px] left-[42px] top-0 absolute bg-white rounded-[30px] shadow" />
-		<div class="w-[241px] h-[254px] left-[333px] top-0 absolute bg-white rounded-[30px] shadow" />
-		<div
-			class="w-[241px] h-[247px] left-[624px] top-[7px] absolute bg-white rounded-[30px] shadow"
-		/>
-	</div>
-	<div class="w-[453px] h-[100px] left-[313px] top-[1430px] absolute">
-		<div class="w-[453px] h-[100px] left-0 top-0 absolute bg-white bg-opacity-50 rounded-[20px]" />
-		<div class="left-[151px] top-[17px] absolute text-black text-opacity-50 text-[50px] font-bold">
-			Claim
+<main>
+	<h1 class="title">Freshie Walk</h1>
+
+	<section class="app">
+		<div class="profile-info">
+			<h2>John Doe</h2>
+			<h2>jdoe@up.edu.ph</h2>
 		</div>
-	</div>
-	<div class="w-[374px] h-[100px] left-[134px] top-[294px] absolute">
-		<div class="left-[137px] top-[27px] absolute text-teal-950 text-[32px] font-light">
-			Brymer Meneses
+
+		<div class="camera-preview">
+
 		</div>
-		<img
-			class="w-[100px] h-[100px] left-0 top-0 absolute rounded-full"
-			src="https://via.placeholder.com/100x100"
-			alt=""
-		/>
-	</div>
-	<div class="left-[223px] top-[81px] absolute text-teal-950 text-8xl font-normal">
-		Freshie Walk
-	</div>
-</div>
+	</section>
+
+</main>
+
+<style>
+
+	h1 {
+		font-family: 'Poppins', sans-serif;
+		font-size: x-large;
+		font-weight: bold;
+	}
+
+	main {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	section.app {
+		background-color: #DFE6EC;
+		width: 80vw;
+		height: 70vh;
+		border-radius: 1rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+	}
+
+	.profile-info {
+
+	}
+
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,31 +10,7 @@
 	<title>Freshie Walk</title>
 	<meta name="description" content="COMSCI@UP.BAG" />
 </svelte:head>
-<div class="w-[1080px] h-[1920px] justify-center items-center gap-2.5 inline-flex">
-	<div class="w-[1080px] h-[2276px] relative">
-		<div
-			class="w-[1000px] h-[870px] left-[54px] top-[1406px] absolute bg-violet-700 bg-opacity-50 rounded-full blur-[1000px]"
-		/>
-	</div>
-	<div class="flex-col justify-center items-center gap-[68px] inline-flex">
-		<div class="text-teal-950 text-8xl font-normal">Freshie Walk</div>
-		<div class="w-[810px] h-[130px] relative">
-			<div
-				class="w-[810px] h-[130px] left-0 top-0 absolute bg-white bg-opacity-50 rounded-[20px]"
-			/>
-			<div class="w-[610px] h-[71px] left-[100px] top-[32px] absolute">
-				<button
-					class="left-0 top-0 absolute text-violet-900 text-opacity-50 text-[50px] font-bold"
-					on:click={handleSignIn}
-				>
-					Login with Google
-				</button>
-				<img
-					class="w-[70px] h-[70px] left-[540px] top-[1px] absolute"
-					src="https://via.placeholder.com/70x70"
-					alt=""
-				/>
-			</div>
-		</div>
-	</div>
-</div>
+
+<h1>
+	Hello world
+</h1>
