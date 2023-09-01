e01a12bbacd0beebb63d187a6a9d53f111986236
Daniel David N. Bador <danieldavidbador@yahoo.com>
2023-09-01 21:06:00 +0800

    add prisma

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,14 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GoogleProvider from '@auth/core/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
-// import { MongoDBAdapter } from '@auth/mongodb-adapter';
-// import clientPromise from '$lib/database/clientPromise';
+import { prisma } from '$lib/prisma';
+
 import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
-const authorization: Handle = async ({ event, resolve }) => {
+const redirects: Handle = async ({ event, resolve }) => {
 	const session = await event.locals.getSession();
 	if (!event.url.pathname.startsWith('/login')) {
 		if (!session) {
@@ -24,8 +26,8 @@ const authorization: Handle = async ({ event, resolve }) => {
 export const handle: Handle = sequence(
 	SvelteKitAuth({
 		trustHost: true,
-		providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })]
-		// adapter: MongoDBAdapter(clientPromise, { databaseName: NODE_ENV }),
+		providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_CLIENT_SECRET })],
+		adapter: PrismaAdapter(prisma)
 	}),
-	authorization
+	redirects
 );
