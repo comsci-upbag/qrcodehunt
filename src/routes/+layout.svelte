b7eb9b72662acd97732c1ca4977eee813275f2e9
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-03-19 16:57:26 +0800

    Add preloading of available card images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import './styles.css';
+	import { availableCardImages } from '$lib/globals';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		availableCardImages.forEach((card) => {
+			new Image().src = card;
+		});
+	});
 </script>
 
 <slot />
