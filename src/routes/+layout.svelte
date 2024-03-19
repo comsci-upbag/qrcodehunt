d931d0f1cd87e625ee2644342967834f751c482f
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-03-20 01:39:46 +0800

    Add loading animation and hide background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,14 +17,24 @@
 </script>
 
 {#await waitBackground}
+	<img src="$lib/assets/background.png" alt="" class="hidden" />
 	<div class="loading">
-		<h1>Loading...</h1>
+		<div class="lds-ripple">
+			<div></div>
+			<div></div>
+		</div>
 	</div>
 {:then}
 	<slot />
 {/await}
 
 <style>
+	.hidden {
+		position: absolute;
+		top: -9999px;
+		left: -9999px;
+		opacity: 0;
+	}
 	.loading {
 		height: 100vh;
 		width: 100vw;
@@ -33,8 +43,50 @@
 		align-items: center;
 		background: black;
 	}
-
-	h1 {
-		color: white;
+	.lds-ripple {
+		display: inline-block;
+		position: relative;
+		width: 80px;
+		height: 80px;
+	}
+	.lds-ripple div {
+		position: absolute;
+		border: 4px solid #fff;
+		opacity: 1;
+		border-radius: 50%;
+		animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+	}
+	.lds-ripple div:nth-child(2) {
+		animation-delay: -0.5s;
+	}
+	@keyframes lds-ripple {
+		0% {
+			top: 36px;
+			left: 36px;
+			width: 0;
+			height: 0;
+			opacity: 0;
+		}
+		4.9% {
+			top: 36px;
+			left: 36px;
+			width: 0;
+			height: 0;
+			opacity: 0;
+		}
+		5% {
+			top: 36px;
+			left: 36px;
+			width: 0;
+			height: 0;
+			opacity: 1;
+		}
+		100% {
+			top: 0px;
+			left: 0px;
+			width: 72px;
+			height: 72px;
+			opacity: 0;
+		}
 	}
 </style>
